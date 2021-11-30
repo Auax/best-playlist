481d8f3be66c00e85a8a265219fb74f14bbadf5f
Ibai Farina <ibaifo8@gmail.com>
2021-11-30 18:33:18 +0100

    honorific mention

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,6 +24,8 @@ const Home = () => {
                 <iframe src="https://open.spotify.com/embed/playlist/4PcuuCbLfVGqh2gsv1ZYbh?utm_source=generator" title="Playlist" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                 <a href="https://open.spotify.com/playlist/4PcuuCbLfVGqh2gsv1ZYbh?si=1c5ce55a45a44f88" target="_blank"
                     rel="noreferrer" className="add-btn text-base font-semibold text-white">Add to Spotify</a>
+                <h1 className="text-3md text-white font-semibold mt-10">Honorific mention</h1>
+                <iframe src="https://open.spotify.com/embed/playlist/7kIB0YN5eTNIZxgXWt3JSX?utm_source=generator&theme=2" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             </div>
         </div>
     )
